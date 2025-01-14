2996e1b7958bb3c15756314dfb438796f18fb3be
Nimira43 <n.rabone72@aol.com>
2025-01-14 04:49:34 +0000

    Wrote function for decryption

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 KEY = 3
 
 def caesar_encrypt(plain_text):
@@ -11,3 +10,13 @@ def caesar_encrypt(plain_text):
     index = (index + KEY) % len(ALPHABET)
     cipher_text = cipher_text + ALPHABET[index]
   return cipher_text
+
+def caesar_decrypt(cipher_text):
+  plain_text = ''
+
+  for y in cipher_text:
+    index = ALPHABET.find(y)
+    index = (index - KEY) % len(ALPHABET)
+    plain_text = plain_text + ALPHABET[index]
+  return plain_text
+
