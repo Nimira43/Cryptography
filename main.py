240e3d71d977d4846237c917c987ebdfbef7d79a
Nimira43 <n.rabone72@aol.com>
2025-01-14 04:44:01 +0000

    Completed first encryption function

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 KEY = 3
 
 def caesar_encrypt(plain_text):
+  cipher_text = ''
+  plain_text.upper()
+
   for x in plain_text:
     index = ALPHABET.find(x)
+    index = (index + KEY) % len(ALPHABET)
+    cipher_text = cipher_text + ALPHABET[index]
+  return cipher_text
